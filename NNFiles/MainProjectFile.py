954e95cb3e0fae3b098fe886596d66e53a58c67c
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2019-01-31 23:11:06 -0500

    More data

--- NNFiles/MainProjectFile.py
+++ NNFiles/MainProjectFile.py
@@ -1,1 +1,6 @@
+import tensorflow as tf
+import cv2 as opencv
+import numpy as np
+import keras_applications as ks
+import Data
 
--- src/DataFile.py
+++ src/DataFile.py
@@ -2,7 +2,7 @@ import numpy
 import pandas as pd
 
 # Dallas Cowboys Dataset Roster
-df = pd.read_csv('Data/NFCEastDB/DallasCowboysDB.csv', encoding="latin-1", names=['Name', 'Position', 'Team', 'Positions > 1'])
+df = pd.read_csv('Data/NFCDB/NFCEastDB/DallasCowboysDB.csv', encoding="latin-1", names=['Name', 'Position', 'Team', 'Positions > 1'])
 teamroster = []
 teamrosterkeys = []
 count = 0
@@ -18,13 +18,13 @@ for pos in df['Position']:
 teamvalcowboy = "Dallas Cowboys"
 teamvalues = []
 teamvalues.append(teamvalcowboy)
-teamdic = {}
+teamdiccowboys = {}
 
 for i in teamrosterkeys:
-    teamdic = {teamrosterkeys[i - 1]: teamroster[i - 1]}
+    teamdiccowboys = {teamrosterkeys[i - 1]: teamroster[i - 1]}
 
 # Philadelphia Eagles Dataset Roster
-df2 = pd.read_csv('Data/NFCEastDB/PhildelphiaEaglesDB.csv', encoding="latin-1", names=['Name', 'Position', 'Team', 'Position > 1'])
+df2 = pd.read_csv('Data/NFCDB/NFCEastDB/PhildelphiaEaglesDB.csv', encoding="latin-1", names=['Name', 'Position', 'Team', 'Position > 1'])
 teamrostereagles = []
 teamrostereagleskeys = []
 count2 = 0
@@ -44,7 +44,7 @@ for i2 in teamrostereagleskeys:
     teamdiceagles = {teamrostereagleskeys[i2 - 1]: teamrostereagles[i2 - 1]}
 
 # New York Giants Dataset Roster
-df3 = pd.read_csv('Data/NFCEastDB/NewYorkGiantsDB.csv', encoding="latin-1", names=['Name', 'Position', 'Team', 'Position > 1'])
+df3 = pd.read_csv('Data/NFCDB/NFCEastDB/NewYorkGiantsDB.csv', encoding="latin-1", names=['Name', 'Position', 'Team', 'Position > 1'])
 teamrostergiants = []
 teamrostergiantskeys = []
 count3 = 0
@@ -64,7 +64,7 @@ for i3 in teamrostergiantskeys:
     teamdicgiants = {teamrostergiantskeys[i3 - 1]: teamrostergiants[i3 - 1]}
 
 # Washington Redskins Dataset Roster
-df4 = pd.read_csv('Data/NFCEastDB/RedskinsDB.csv', encoding="latin-1", names=['Name', 'Position', 'Team', 'Position > 1'])
+df4 = pd.read_csv('Data/NFCDB/NFCEastDB/RedskinsDB.csv', encoding="latin-1", names=['Name', 'Position', 'Team', 'Position > 1'])
 teamrosterRedskins = []
 teamrosterkeysRedskins = []
 count4 = 0
@@ -75,5 +75,32 @@ for name in df4['Name']:
 
 positionsredskins = []
 for pos4 in df4['Position']:
-    positionsredskins.append(pos4)
+        positionsredskins.append(pos4)
 
+teamvalredskins = "Washington Redskins"
+teamvalues.append(teamvalredskins)
+teamdicredskins = {}
+for i4 in teamrosterkeysRedskins:
+    teamdicredskins = {teamrosterkeysRedskins[i4 - 1]: teamrosterRedskins[i4 - 1]}
+
+# NFC West data reading.
+######################################
+# Reading of data for the Los Angeles Rams Roster
+df5 = pd.read_csv("Data/NFCDB/NFCWestDB/LARamsDB.csv", encoding="latin-1", names=['Name', 'Position', 'Team', 'Position > 1'])
+teamrosterLARams = []
+teamrosterkeysLARams = []
+count5 = 0
+for name in df5['Name']:
+    teamrosterLARams.append(name)
+    count5 = count5 + 1
+    teamrosterkeysLARams.append(count5)
+
+positionsLARams = []
+for pos5 in df5['Position']:
+    positionsLARams.append(pos5)
+
+teamvallarams = "Los Angeles Rams"
+teamvalues.append(teamvallarams)
+teamdiclarams = {}
+for i5 in teamrosterkeysLARams:
+    teamdiclarams = {teamrosterkeysLARams[i5 - 1]: teamrosterLARams[i5 - 1]}
