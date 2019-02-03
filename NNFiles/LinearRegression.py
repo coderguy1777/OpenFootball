719f38d720527bfcc66132bf7390f7f828b611d6
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2019-02-03 17:12:39 -0500

    Add files via upload

---
+++ NNFiles/LinearRegression.py
@@ -0,0 +1,65 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class LinearRegression:
+    def __init__(self, data, data2):
+        self.data = data
+        self.data2 = data2
+        self.linregdic = {}
+        self.xsquaretot = 0
+        self.ysquaretot = 0
+        self.xmeanval = 0
+        self.ymeanval = 0
+        self.xysum = 0
+        self.b1value = 0
+        self.xsum = 0
+        self.ysum = 0
+        self.b0value = 0
+
+    def b1val(self):
+        self.xsum = np.sum(self.data)
+        self.ysum = np.sum(self.data2)
+        self.xmeanval = self.xsum / len(self.data)
+        self.ymeanval = self.ysum / len(self.data2)
+        xsum = np.sum(self.xsum - self.xmeanval)
+        ysum = np.sum(self.ysum - self.ymeanval)
+        self.xysum = np.sum(xsum* ysum)
+        self.xsquaretot = np.sum(self.xsum - self.xmeanval)**2
+        b1 = self.xysum / self.xsquaretot
+        self.b1value = b1
+
+    def b1finaleq(self):
+        npoints = len(self.data) + len(self.data2)
+        part1ofeq = self.xsum * self.ysum / npoints
+        half1ofeq = self.xysum - part1ofeq
+        part2ofeq = self.xsum ** 2 / npoints
+        half2ofeq = self.xsquaretot - part2ofeq
+        finaleq = half1ofeq/half2ofeq
+        self.b1value = finaleq
+
+    def b0val(self):
+        npoints = len(self.data) + len(self.data2)
+        tophalf = self.ysum - (self.b1value * self.xsum)
+        bottomhalf = npoints
+        finaleq = tophalf/bottomhalf
+        self.b0value = finaleq
+        return self.b0value
+
+    def yhat(self, x):
+        li1 = []
+        eq = self.b0value + self.b1value * x
+        li1.append(eq)
+        li2 = []
+        li2.append(x)
+        print(eq)
+        plt.plot(eq, x)
+        return eq
+
+    def linearregressioneq(self):
+        print("y= " + str(self.b0value) + " + " + str(self.b1value) + "x")
+
+    def dataplot(self, dataset, dataset2):
+        plt.plot(dataset, dataset2, 'ro')
+        plt.ylabel("Y Data")
+        plt.xlabel("X Data")
+        plt.show()
--- NNFiles/MainProjectFile.py
+++ NNFiles/MainProjectFile.py
@@ -1,6 +1,22 @@
 import tensorflow as tf
 import cv2 as opencv
 import numpy as np
-import keras_applications as ks
-import Data
+from NNFiles.LinearRegression import LinearRegression
+
+data1 = []
+data2 = []
+for i in range(7):
+    data1.append(np.random.randint(0, 10, size=9))
+for i2 in range(7):
+    data2.append(np.random.randn(20))
+
+
+clf = LinearRegression(data1, data2)
+clf.b1val()
+clf.b1finaleq()
+clf.b0val()
+clf.linearregressioneq()
+for i in data1:
+    clf.yhat(i)
+    clf.dataplot(clf.yhat(i), i)
 
